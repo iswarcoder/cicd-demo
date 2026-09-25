84d2b7c678eedc33c95f0a369e50508304c078a9
Raja <grajeshkumar909058@gmail.com>
2026-09-25 22:48:53 +0530

    Initial CI/CD project

---
+++ app.js
@@ -0,0 +1,18 @@
+const http = require("http");
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {
+        "Content-Type": "text/html"
+    });
+
+    res.end(`
+        <h1>CI/CD Pipeline Successfully Deployed!</h1>
+        <p>Application is running successfully.</p>
+    `);
+});
+
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
